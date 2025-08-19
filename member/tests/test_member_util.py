0e0ef981d48dcafea563d69f6e7c75116f092799
Gazar <kolajokimzee2020@gmailcom>
2025-08-19 11:54:12 +0100

    test

--- member/tests/test_member_util.py
+++ member/tests/test_member_util.py
@@ -1,10 +1,50 @@
-import unittest
+from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+
+from family_info.models import FamilyInfo
+from member.models import Member
+from member.services import get_family_members
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class MemberServiceTests(TestCase):
+
+    def setUp(self):
+        self.family = FamilyInfo.objects.create(
+            tribe="Yoruba Tribe",
+            foods="Rice, Yam",
+            origin="Nigeria",
+            traditions="Traditional ceremonies",
+            additional_notes="Some notes"
+        )
 
 
-if __name__ == '__main__':
-    unittest.main()
+        self.member1 = Member.objects.create(
+            name="John Ada",
+            birth_date="1995-05-12",
+            family_info=self.family,
+        )
+        self.member2 = Member.objects.create(
+            name="micheal",
+            birth_date="1998-09-20",
+            family_info=self.family,
+        )
+
+    def test_get_family_members(self):
+        members = get_family_members(self.family.id)  #expects family_id
+        self.assertIn(self.member1, members)
+        self.assertIn(self.member2, members)
+        self.assertEqual(members.count(), 2)
+
+    def test_update_member(self):
+        self.member1.name = "John Updated"
+        self.member1.save()
+
+        updated_member = Member.objects.get(id=self.member1.id)
+        self.assertEqual(updated_member.name, "John Updated")
+
+    def test_delete_member(self):
+        member_id = self.member2.id
+        self.member2.delete()
+
+        with self.assertRaises(ObjectDoesNotExist):
+            Member.objects.get(id=member_id)
